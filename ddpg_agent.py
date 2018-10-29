6d7895a7b51d56ce0813d34bac767c4f11eb46ab
JeroenSweerts <jeroensweerts@hotmail.com>
2018-10-29 21:05:44 +0100

    final commit

--- ddpg_agent.py
+++ ddpg_agent.py
@@ -9,20 +9,21 @@ import torch
 import torch.nn.functional as F
 import torch.optim as optim
 
-BUFFER_SIZE = int(1e5)  # replay buffer size
-BATCH_SIZE = 1280        # minibatch size
-GAMMA = 0.99            # discount factor
+BUFFER_SIZE = int(1e6)  # replay buffer size
+BATCH_SIZE = 1024      # minibatch size
+GAMMA = 0.99           # discount factor
 TAU = 1e-3              # for soft update of target parameters
-LR_ACTOR = 1e-4         # learning rate of the actor 
+LR_ACTOR = 5e-4         # learning rate of the actor 
 LR_CRITIC = 1e-3        # learning rate of the critic
-WEIGHT_DECAY = 0.01        # L2 weight decay
+WEIGHT_DECAY = 0      # L2 weight decay
+NUM_AGENTS = 20
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 class Agent():
     """Interacts with and learns from the environment."""
     
-    def __init__(self, state_size, action_size, random_seed):
+    def __init__(self, state_size, action_size, random_seed, filenameActor=None, filenameCritic=None):
         """Initialize an Agent object.
         
         Params
@@ -44,22 +45,38 @@ class Agent():
         self.critic_local = Critic(state_size, action_size, random_seed).to(device)
         self.critic_target = Critic(state_size, action_size, random_seed).to(device)
         self.critic_optimizer = optim.Adam(self.critic_local.parameters(), lr=LR_CRITIC, weight_decay=WEIGHT_DECAY)
+        
+        if filenameActor:
+            weights = torch.load(filenameActor)
+            self.actor_local.load_state_dict(weights)
+            self.actor_target.load_state_dict(weights)
+            
+        if filenameCritic:
+            weights = torch.load(filenameCritic)
+            self.critic_local.load_state_dict(weights)
+            self.critic_target.load_state_dict(weights)
 
         # Noise process
         self.noise = OUNoise(action_size, random_seed)
 
         # Replay memory
         self.memory = ReplayBuffer(action_size, BUFFER_SIZE, BATCH_SIZE, random_seed)
+        
     
-    def step(self, state, action, reward, next_state, done):
+    def step(self, state, action, reward, next_state, done, timestep):
         """Save experience in replay memory, and use random sample from buffer to learn."""
         # Save experience / reward
-        self.memory.add(state, action, reward, next_state, done)
+        for i in range(NUM_AGENTS):
+            self.memory.add(state[i], action[i], reward[i], next_state[i], done[i])
 
         # Learn, if enough samples are available in memory
-        if len(self.memory) > BATCH_SIZE:
-            experiences = self.memory.sample()
-            self.learn(experiences, GAMMA)
+        if timestep%10 == 0:
+            if len(self.memory) > BATCH_SIZE:
+                for i in range(10):
+                    experiences = self.memory.sample()
+                    self.learn(experiences, GAMMA)
+       
+                
 
     def act(self, state, add_noise=True):
         """Returns actions for given state as per current policy."""
@@ -69,7 +86,8 @@ class Agent():
             action = self.actor_local(state).cpu().data.numpy()
         self.actor_local.train()
         if add_noise:
-            action += self.noise.sample()
+            for i in range(NUM_AGENTS):
+                action[i] += self.noise.sample()
         return np.clip(action, -1, 1)
 
     def reset(self):
@@ -101,14 +119,15 @@ class Agent():
         # Minimize the loss
         self.critic_optimizer.zero_grad()
         critic_loss.backward()
+        torch.nn.utils.clip_grad_norm(self.critic_local.parameters(), 1)  #use gradient clipping
         self.critic_optimizer.step()
 
         # ---------------------------- update actor ---------------------------- #
         # Compute actor loss
         actions_pred = self.actor_local(states)
-        actor_loss = -self.critic_local(states, actions_pred).mean()
+        actor_loss = -self.critic_local(states, actions_pred).mean() ##############
         # Minimize the loss
-        self.actor_optimizer.zero_grad()
+        self.actor_optimizer.zero_grad()         
         actor_loss.backward()
         self.actor_optimizer.step()
 
--- model.py
+++ model.py
@@ -22,11 +22,15 @@ class Actor(nn.Module):
             fc1_units (int): Number of nodes in first hidden layer
             fc2_units (int): Number of nodes in second hidden layer
         """
+        proba = 0.2
         super(Actor, self).__init__()
         self.seed = torch.manual_seed(seed)
+        self.bn1 = nn.BatchNorm1d(state_size)   
         self.fc1 = nn.Linear(state_size, fc1_units)
+        self.conv2_drop1 = nn.Dropout(p=proba)
         self.fc2 = nn.Linear(fc1_units, fc2_units)
-        self.fc3 = nn.Linear(fc2_units, action_size)
+        self.conv2_drop2 = nn.Dropout(p=proba)
+        self.fc3 = nn.Linear(fc2_units, action_size)        
         self.reset_parameters()
 
     def reset_parameters(self):
@@ -36,9 +40,10 @@ class Actor(nn.Module):
 
     def forward(self, state):
         """Build an actor (policy) network that maps states -> actions."""
-        x = F.relu(self.fc1(state))
-        x = F.relu(self.fc2(x))
-        return F.tanh(self.fc3(x))
+        state = self.bn1(state)
+        x = F.relu(self.conv2_drop1(self.fc1(state)))
+        x = F.relu(self.conv2_drop2(self.fc2(x)))
+        return F.tanh(self.fc3(x))        
 
 
 class Critic(nn.Module):
@@ -54,11 +59,14 @@ class Critic(nn.Module):
             fcs1_units (int): Number of nodes in the first hidden layer
             fc2_units (int): Number of nodes in the second hidden layer
         """
+        proba = 0.2
         super(Critic, self).__init__()
         self.seed = torch.manual_seed(seed)
-        self.fcs1 = nn.Linear(state_size, fcs1_units)
+        self.bn1 = nn.BatchNorm1d(state_size)
+        self.fcs1 = nn.Linear(state_size, fcs1_units)        
         self.fc2 = nn.Linear(fcs1_units+action_size, fc2_units)
-        self.fc3 = nn.Linear(fc2_units, 1)
+        self.conv2_drop2 = nn.Dropout(p=proba)
+        self.fc3 = nn.Linear(fc2_units, 1)        
         self.reset_parameters()
 
     def reset_parameters(self):
@@ -68,7 +76,8 @@ class Critic(nn.Module):
 
     def forward(self, state, action):
         """Build a critic (value) network that maps (state, action) pairs -> Q-values."""
+        state = self.bn1(state)
         xs = F.relu(self.fcs1(state))
         x = torch.cat((xs, action), dim=1)
-        x = F.relu(self.fc2(x))
+        x = F.relu(self.conv2_drop2(self.fc2(x)))        
         return self.fc3(x)
